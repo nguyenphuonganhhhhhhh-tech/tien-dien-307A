0c0e1ba408ae3d3730d6c745cad1eee9de029837
nguyenphuonganhhhhhhh-tech <nguyenphuonganhhhhhhh@gmail.com>
2026-09-14 14:50:50 +0700

    Update appc1.py

--- appc1.py
+++ appc1.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Tính Tiền Điện Phòng 307A", page_icon="⚡", layout="centered")
 
-st.title("⚡ Tính Tiền Điện Phòng Trọ")
+st.title("Tính Tiền Điện P307A")
 st.write("Công cụ phân bổ tiền điện công bằng theo số ngày ở thực tế của từng thành viên.")
 
 # 1. Nhập tổng tiền điện
